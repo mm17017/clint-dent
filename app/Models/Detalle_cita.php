c97cefd8356566c72291fba1ddbdc7cdec4600ac
cmargueiz <c.margueiz@gmail.com>
2021-05-25 16:16:27 -0600

    relaciones de detalle de cita

--- app/Models/Detalle_cita.php
+++ app/Models/Detalle_cita.php
@@ -7,5 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class detalle_cita extends Model
 {
+    public function usuarios()
+    {
+        return $this->belongsTo(User::class);
+    }
 
+    public function jornadas()
+    {
+        return $this->belongsTo(Jornada::class);
+    }
+
+    public function servicios()
+    {
+        return $this->belongsTo(Servicio::class);
+    }
+
+    public function estado_citas()
+    {
+        return $this->belongsTo(Estado_cita::class);
+    }
 }
