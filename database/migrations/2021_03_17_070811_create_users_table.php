35d2a253965e00790ee26d6737e87502928475e7
Guillex99 <mf17006@ues.edu.sv>
2021-05-26 22:32:27 -0600

    modificacion del tamano del campo de password de migracion user_table

--- database/migrations/2021_03_17_070811_create_users_table.php
+++ database/migrations/2021_03_17_070811_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email', 100)->unique();
             $table->string('login', 10);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 45);
+            $table->string('password',255);
             $table->string('lastname_usuario', 50);
             $table->string('name_usuario', 50);
             $table->date('fecha_nacimiento');
