dcc6c9f550617e53d553c3ab6009c4a81bfae69c
ElmerInteriano <elmerinteriano211@gmail.com>
2021-05-27 10:12:58 -0600

    Agregando modificaciones a la vista Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ClintDent</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -12,8 +12,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #cff4d2;
+                color: #205072;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -37,7 +37,7 @@
             .top-right {
                 position: absolute;
                 right: 10px;
-                top: 18px;
+                top: 20px;
             }
 
             .content {
@@ -49,7 +49,7 @@
             }
 
             .links > a {
-                color: #636b6f;
+                color: #329d9c;
                 padding: 0 25px;
                 font-size: 13px;
                 font-weight: 600;
@@ -61,30 +61,71 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            .container-logo{
+                /* max-width: 100%; */
+                width: auto;
+                height: auto;
+                max-width: 40px;
+                max-height: 40px;
+            }
+            /* .space{
+                justify-content: space-between;
+            } */
+            .top-left {
+                position: absolute;
+                left: 10px;
+                top: 10px;
+            }
+            .box{
+                width: 100vw;
+                height: 60px;
+                background-color: #fff;
+                position: absolute;
+                top: 0px;
+            }
+            .contenedor{
+                max-width: 1200px;
+            }
+            .logo-main{
+                
+                /* width: auto;
+                height: auto; */
+                width: 400px;
+                height: 400px;
+            }
         </style>
     </head>
     <body>
+        
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+            
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+                @if (Route::has('login'))
+                    <div class="box">
+                        <div class="top-left ">
+                            <img class="container-logo" src="{{URL::asset('img/ClintDent.png')}}" alt="">
+                        </div>
+                        <div class="top-right links">
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Login</a>
 
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Register</a>
+                            @endif
+                        @endauth
+                        </div>
+                    </div>
+                @endif
+            
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <img class="logo-main" src="{{URL::asset('img/Logo.png')}}" alt="">
 
-                <div class="links">
+                <h1>Siempre Lo Mejor Para Tu Salud</h1>
+            
+
+                {{-- <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
@@ -92,8 +133,8 @@
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> 
+                </div> --}}
             </div>
         </div>
     </body>
