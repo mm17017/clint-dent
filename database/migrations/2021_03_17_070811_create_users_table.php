f96b2dbb71dabd8844fa2d22c55fc3fdc257fb90
cmargueiz <c.margueiz@gmail.com>
2021-05-27 01:02:40 -0600

    eliminacion de campo de login

--- database/migrations/2021_03_17_070811_create_users_table.php
+++ database/migrations/2021_03_17_070811_create_users_table.php
@@ -16,7 +16,6 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email', 100)->unique();
-            $table->string('login', 10);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password',255);
             $table->string('lastname_usuario', 50);
