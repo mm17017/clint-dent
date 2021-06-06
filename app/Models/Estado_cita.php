2088673550b4d765c1c36076222a7b2696e74882
cmargueiz <c.margueiz@gmail.com>
2021-06-06 12:21:45 -0600

    se agregaron campos filliables

--- app/Models/Estado_cita.php
+++ app/Models/Estado_cita.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Estado_cita extends Model
 {
+    protected $fillable = ['estado','descripcion'];
     public function detalle_citas(){
         return $this->hasMany(detalle_citas::class);
     }
