0fa70a1201cc897ac7b473fd78923ce74113a037
cmargueiz <c.margueiz@gmail.com>
2021-06-14 14:13:23 -0600

    agregue vista y controlador de usurios

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/inicio','InicioController@index')->name('inicio');
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user','UserController@index')->name('user');
 
 Auth::routes();
 
