4173c67c6a65d66c1f07e761b622893b0420dae1
ElmerInteriano <elmerinteriano211@gmail.com>
2021-06-14 10:39:47 -0600

    Servicios

--- app/Models/Servicio.php
+++ app/Models/Servicio.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Servicio extends Model
 {
+    protected $table = 'servicios';
+
+    protected $fillable =  ['descripcion','precio','tiempo_tratamiento'];
+    protected $hidden =  ['created_at','updated_at'];
     public function detalle_cita_servicio(){
         return $this->hasMany(detalle_cita_servicio::class);
     }
--- database/seeds/ServicioSeeder.php
+++ database/seeds/ServicioSeeder.php
@@ -12,6 +12,72 @@ class ServicioSeeder extends Seeder
      */
     public function run()
     {
-        factory(Servicio::class, 10)->create();
+        Servicio::create([
+            'descripcion'=>'Limpieza Dental',
+            'precio'=>'35.00',
+            'tiempo_tratamiento'=>'00:40:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Resina Dental',
+            'precio'=>'40.00',
+            'tiempo_tratamiento'=>'00:50:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Endodoncia',
+            'precio'=>'155.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Coronas Dentales',
+            'precio'=>'285.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Implantes dentales',
+            'precio'=>'800.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Extraccion',
+            'precio'=>'50.00',
+            'tiempo_tratamiento'=>'00:40:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Guarda Oclusal',
+            'precio'=>'75.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Puentes Dentales',
+            'precio'=>'860.00',
+            'tiempo_tratamiento'=>'02:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Brackets Tradicionales',
+            'precio'=>'335.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Brackets Esteticos',
+            'precio'=>'675.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Brackets Autoligados',
+            'precio'=>'575.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Invisaling',
+            'precio'=>'1255.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+        Servicio::create([
+            'descripcion'=>'Blanqueamiento Dental',
+            'precio'=>'200.00',
+            'tiempo_tratamiento'=>'01:00:00',
+        ]);
+
+        //factory(Servicio::class, 10)->create();
     }
 }
