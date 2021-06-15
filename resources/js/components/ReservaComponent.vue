bbcc7ee43a53033708fb3c056c81c11b19b0889f
Guillex99 <mf17006@ues.edu.sv>
2021-06-14 18:30:04 -0600

    Se edito el ReservaComponent

--- resources/js/components/ReservaComponent.vue
+++ resources/js/components/ReservaComponent.vue
@@ -1,10 +1,76 @@
 <template>
-	<div class="container">
-		Hola mundo feo, soy el componente que va a modificar guille
-	</div>	
+    <form method="POST">
+        <div class="form-group row">
+            <label
+                for="descripcion"
+                class="col-md-4 col-form-label text-md-right color-blanco"
+                >Descripcion</label
+            >
 
+            <div class="col-md-6">
+                <input
+                    id="descripcion"
+                    placeholder="Detalla tu cita"
+                    type="text"
+                    class="form-control"
+                    name="name_usuario"
+                    autofocus
+                />
+            </div>
+        </div>
+        <div class="form-group row">
+            <label
+                for="fecha_cita"
+                class="col-md-4 col-form-label text-md-right color-blanco"
+                >Fecha de cita</label
+            >
+            <div class="col-md-6">
+                <input
+                    id="fecha_cita"
+                    type="date"
+                    class="form-control"
+                    name="lastname_usuario"
+                />
+            </div>
+        </div>
+        <div class="form-group row">
+            <label
+                for="hora_cita"
+                class="col-md-4 col-form-label text-md-right color-blanco"
+                >Hora programada</label
+            >
+
+            <div class="col-md-6">
+                <select
+                    class="custom-select"
+                    multiple
+                    id="servicios"
+                    v-model="cita.serviciosSeleccionados"
+                >
+                    <option
+                        v-for="servicio in servicios"
+                        :value="servicio.id"
+                        :key="servicio.id"
+                        >{{ servicio.id }} - {{ servicio.descripcion }}</option
+                    >
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group row mb-0">
+            <div class="col-md-6 offset-md-4 text-right">
+                <button type="submit" class="btn btn-primary">
+                    Reservar
+                </button>
+            </div>
+        </div>
+    </form>
 </template>
-<script></script>
-<style scoped>
-	
-</style>
+
+<script>
+
+
+</script>
+
+
+<style scoped></style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,31 +3,33 @@
 @section('content')
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-
-<div class="contenedor">
+<h1 class="h1-inicio">
+    Bienvenido a nuestro sistema de reserva en línea
+</h1>
+<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Citas') }}</div>
-
-                <div class="card-body">
+            <div class="">
+                <div class="card-body contenedor sombra gradiente-form col-form-label">
+                    <h2 class="text-center padding-bottom color-blanco">Reserva tu cita</h2>
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    <div class="sesion">
-                        <p class="text-left align-content-lg-around">
-                            Bienvenido: {{ Auth::user()->name_usuario }} 
-                        </p>
+                    <div class="componente">
+                        <reserva-component />
                     </div>
-                    
+                    <!-- <div class="sesion">
+                        <p class="text-left align-content-lg-around">
+                            Bienvenido: {{ Auth::user()->name_usuario }}
+                        </p>
+                    </div> -->
+
                 </div>
-                
-                 <example-component/>
-                <reserva-component/>
-                
+
+
             </div>
         </div>
     </div>
